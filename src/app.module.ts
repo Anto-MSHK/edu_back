50da40fe97d63ab4e76df3f9a01827080c881a9d
Anto-MSHK <anjiilop25@gmail.com>
2023-03-19 00:06:58 +0300

    111

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,8 +19,9 @@ dotenv.config();
   imports: [
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
+      host: process.env.DB_HOST,
+      uri: process.env.DB_URL,
+      port: +process.env.DB_PORT,
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
